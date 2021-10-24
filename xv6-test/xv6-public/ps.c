02db0ea7b6323a43e42abdba7ce452cf5410e8ff
unknown <luisalazago@gmail.com>
2021-10-24 16:25:49 -0500

    Uploading real files

---
+++ xv6-test/xv6-public/ps.c
@@ -0,0 +1,12 @@
+/* This file call specifically the cps function */
+
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+#include "syscall.h"
+
+int main(void) {
+    cps();
+    exit();
+}
